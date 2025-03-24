d33c0f02ac1a568d786a914e0a517ecc1c206954
Dimitri-Bit <dimitrijefustic0@gmail.com>
2025-03-24 18:32:11 +0100

    auth design

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../hooks/use-auth";
+import registerImage from "../assets/undraw_login.svg";
 
 export const RegisterPage = () => {
   const { user, setUser, register, message } = useAuth();
@@ -14,48 +15,55 @@ export const RegisterPage = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
-          Register
-        </h1>
-        {message && <p className="text-center text-red-500 mb-4">{message}</p>}
-        <div>
-          <input
-            type="text"
-            placeholder="Username"
-            value={user.username}
-            onChange={(e) => setUser({ ...user, username: e.target.value })}
-            className="w-full p-3 border border-gray-300 rounded-md mb-4"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            value={user.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-            className="w-full p-3 border border-gray-300 rounded-md mb-4"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={user.password}
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
-            className="w-full p-3 border border-gray-300 rounded-md mb-4"
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            value={passwordConfirm}
-            onChange={(e) => setPasswordConfirm(e.target.value)}
-            className="w-full p-3 border border-gray-300 rounded-md mb-4"
-          />
+    <div className="flex justify-center items-center h-screen bg-black">
+      <div className="w-5/6 md:w-3/6 bg-gray-900 p-8 rounded-lg shadow-lg flex items-center">
+        <div className="w-6/6 hidden lg:block">
+          <img src={registerImage} alt="" />
         </div>
-        <button
-          onClick={handleRegister}
-          className="w-full p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-        >
-          Register
-        </button>
+        <div>
+          <h1 className="text-2xl font-bold text-center text-white mb-6 uppercase">
+            Register
+          </h1>
+          {message && (
+            <p className="text-center text-red-500 mb-4">{message}</p>
+          )}
+          <div>
+            <input
+              type="text"
+              placeholder="Username"
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              className="w-full p-3 border border-purple-700 rounded-md mb-4 text-white"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              className="w-full p-3 border border-purple-700 rounded-md mb-4 text-white"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              className="w-full p-3 border border-purple-700 rounded-md mb-4 text-white"
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+              className="w-full p-3 border border-purple-700 rounded-md mb-4 text-white"
+            />
+          </div>
+          <button
+            onClick={handleRegister}
+            className="w-full p-3  bg-purple-900 text-white rounded-md hover:bg-purple-800 transition-all duration-300 ease-in-out hover:cursor-pointer mb-4"
+          >
+            Register
+          </button>
+        </div>
       </div>
     </div>
   );
