ea2caf5701876ae4b4fcfc8958a2270e89374964
Popović Jovan <jovan6.p@gmail.com>
2025-03-24 10:12:38 +0100

    Update main.jsx

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,4 +1,4 @@
-import { NavbarComponent } from "../components/Navbar";
+import { NavbarComponent } from "../components/navbar";
 import { Outlet } from "react-router-dom";
 
 export const RootLayout = () => {
