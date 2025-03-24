d49194c15c59ff0f13736def4d5a55909ad256b3
Dimitri-Bit <dimitrijefustic0@gmail.com>
2025-03-24 16:23:34 +0100

    resolved

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,33 +5,27 @@ import logoImage from "../assets/logo.png";
 export const NavbarComponent = () => {
   const [isArrowVisible, setArrowVisible] = useState(false);
   const [isBurgerVisible, setBurgerVisible] = useState(false);
-
   useEffect(() => {
     const handleScroll = () => {
       setArrowVisible(window.scrollY > 100);
     };
-
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
-
   const handleBurgerClick = () => {
     setBurgerVisible(!isBurgerVisible);
   };
-
   const navItems = [
     { path: "/", label: "Home" },
     { path: "/parties", label: "Parties" },
     { path: "/about", label: "About" },
   ];
-
   const renderNavLink = (path, label, className = "", onClick = null) => (
     <NavLink
       className={({ isActive }) =>
@@ -43,7 +37,6 @@ export const NavbarComponent = () => {
       {label}
     </NavLink>
   );
-
   return (
     <>
       <nav className="navbar m-auto px-12 pt-8 pb-7 bg-gray-900">
@@ -64,7 +57,6 @@ export const NavbarComponent = () => {
               ></i>
             </button>
           </div>
-
           <ul className="space-x-4 primary-nav-list hidden md:block">
             {navItems.map((item) => (
               <li key={item.path} className="inline">
@@ -74,7 +66,6 @@ export const NavbarComponent = () => {
           </ul>
         </div>
       </nav>
-
       <div
         className={`mobile-nav bg-gray-800 transition-all duration-300 ease-in-out ${
           isBurgerVisible ? "max-h-64 opacity-100" : "max-h-0 opacity-0"
@@ -97,28 +88,7 @@ export const NavbarComponent = () => {
         </ul>
       </div>
 
-      <div
-        className={`mobile-nav bg-gray-800 transition-all duration-300 ease-in-out ${
-          isBurgerVisible ? "max-h-64 opacity-100" : "max-h-0 opacity-0"
-        } md:hidden`}
-      >
-        <ul className="py-4 px-12">
-          {navItems.map((item) => (
-            <li key={item.path} className="py-2">
-              {renderNavLink(item.path, item.label, "block", handleBurgerClick)}
-            </li>
-          ))}
-          <li className="py-2 flex space-x-4">
-            <NavLink className="nav-item" to="/" onClick={handleBurgerClick}>
-              <i className="fa-solid fa-toggle-off text-xl"></i>
-            </NavLink>
-            <NavLink className="nav-item" to="/" onClick={handleBurgerClick}>
-              <i className="fa-solid fa-user text-xl"></i>
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-
+      <div className="nav-border pb-1"></div>
       <button
         onClick={scrollToTop}
         className={`arrow-up m-5 text-l p-3 rounded ${
