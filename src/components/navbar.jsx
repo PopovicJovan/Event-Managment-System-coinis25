198b00927656e222ea98ec42554b380fdf359343
PopovicJovan <jovan6.p@gmail.com>
2025-03-24 15:25:13 +0100

    navbar syntax error fixes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -33,126 +33,81 @@ export const NavbarComponent = () => {
   ];
 
   const renderNavLink = (path, label, className = "", onClick = null) => (
-    <NavLink
-      className={({ isActive }) =>
-        `${isActive ? "active-nav" : "inactive-nav"} nav-item ${className}`
-      }
-      to={path}
-      onClick={onClick}
-    >
-      {label}
-    </NavLink>
+      <NavLink
+          className={({ isActive }) =>
+              `${isActive ? "active-nav" : "inactive-nav"} nav-item ${className}`
+          }
+          to={path}
+          onClick={onClick}
+      >
+        {label}
+      </NavLink>
   );
 
   return (
-    <>
+      <>
+        <nav className="navbar m-auto px-12 pt-8 pb-7 bg-gray-900">
+          <div className="list-container flex justify-between w-5/7 m-auto">
+            <div className="logo-container flex justify-between">
+              <Link to="/">
+                <img className="w-25" src={logoImage} alt="Logo" />
+              </Link>
+              <button
+                  onClick={handleBurgerClick}
+                  className="burger-button md:hidden"
+                  aria-label="Toggle mobile menu"
+              >
+                <i
+                    className={`fa-solid ${
+                        isBurgerVisible ? "fa-times" : "fa-bars"
+                    } text-2xl`}
+                ></i>
+              </button>
+            </div>
 
-      <nav className="navbar m-auto px-12 pt-8 pb-7 bg-gray-900">
-        <div className="list-container flex justify-between w-5/7 m-auto">
-          <div className="logo-container flex justify-between">
-            <Link to="/">
-              <img className="w-25" src={logoImage} alt="Logo" />
-            </Link>
-            <button
-              onClick={handleBurgerClick}
-              className="burger-button md:hidden"
-              aria-label="Toggle mobile menu"
-            >
-              <i
-                className={`fa-solid ${
-                  isBurgerVisible ? "fa-times" : "fa-bars"
-                } text-2xl`}
-              ></i>
-            </button>
+            <ul className="space-x-4 primary-nav-list hidden md:block">
+              {navItems.map((item) => (
+                  <li key={item.path} className="inline">
+                    {renderNavLink(item.path, item.label)}
+                  </li>
+              ))}
+            </ul>
           </div>
+        </nav>
 
-          <ul className="space-x-4 primary-nav-list hidden md:block">
+        <div
+            className={`mobile-nav bg-gray-800 transition-all duration-300 ease-in-out ${
+                isBurgerVisible ? "max-h-64 opacity-100" : "max-h-0 opacity-0"
+            } md:hidden`}
+        >
+          <ul className="py-4 px-12">
             {navItems.map((item) => (
-              <li key={item.path} className="inline">
-                {renderNavLink(item.path, item.label)}
-              </li>
+                <li key={item.path} className="py-2">
+                  {renderNavLink(item.path, item.label, "block")}
+                </li>
             ))}
-
-      <nav className="navbar m-auto px-12 pt-8 pb-7">
-        <div className="flex justify-between w-5/7 m-auto">
-          <img className="w-25" src={logoImage} alt="" />
-          <ul className="space-x-7">
-            <li className="inline">
-              <NavLink
-                className={({ isActive }) =>
-                  isActive
-                    ? "active-nav nav-item rounded-xl"
-                    : "inactive-nav nav-item rounded-xl"
-                }
-                to="/"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="inline">
-              <NavLink
-                className={({ isActive }) =>
-                  isActive
-                    ? "active-nav nav-item rounded-xl"
-                    : "inactive-nav nav-item rounded-xl"
-                }
-                to="/parties"
-              >
-                Parties
-              </NavLink>
-            </li>
-
-
-          </ul>
-
-          <ul className="secondary-nav-list hidden md:block">
-            <li className="inline">
-              <NavLink className="nav-item" to="/">
+            <li className="py-2 flex space-x-4">
+              <NavLink className="nav-item" to="/" onClick={handleBurgerClick}>
                 <i className="fa-solid fa-toggle-off text-xl"></i>
               </NavLink>
-            </li>
-            <li className="inline">
-              <NavLink className="nav-item" to="/">
+              <NavLink className="nav-item" to="/" onClick={handleBurgerClick}>
                 <i className="fa-solid fa-user text-xl"></i>
               </NavLink>
             </li>
           </ul>
         </div>
-      </nav>
 
-      <div
-        className={`mobile-nav bg-gray-800 transition-all duration-300 ease-in-out ${
-          isBurgerVisible ? "max-h-64 opacity-100" : "max-h-0 opacity-0"
-        } md:hidden`}
-      >
-        <ul className="py-4 px-12">
-          {navItems.map((item) => (
-            <li key={item.path} className="py-2">
-              {renderNavLink(item.path, item.label, "block")}
-            </li>
-          ))}
-          <li className="py-2 flex space-x-4">
-            <NavLink className="nav-item" to="/" onClick={handleBurgerClick}>
-              <i className="fa-solid fa-toggle-off text-xl"></i>
-            </NavLink>
-            <NavLink className="nav-item" to="/" onClick={handleBurgerClick}>
-              <i className="fa-solid fa-user text-xl"></i>
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+        <div className="nav-border pb-1"></div>
 
-      <div className="nav-border pb-1"></div>
-
-      <button
-        onClick={scrollToTop}
-        className={`arrow-up m-5 text-l p-3 rounded ${
-          isArrowVisible ? "opacity-100" : "opacity-0"
-        }`}
-        aria-label="Scroll to top"
-      >
-        <i className="fa-solid fa-arrow-up"></i>
-      </button>
-    </>
+        <button
+            onClick={scrollToTop}
+            className={`arrow-up m-5 text-l p-3 rounded ${
+                isArrowVisible ? "opacity-100" : "opacity-0"
+            }`}
+            aria-label="Scroll to top"
+        >
+          <i className="fa-solid fa-arrow-up"></i>
+        </button>
+      </>
   );
 };
