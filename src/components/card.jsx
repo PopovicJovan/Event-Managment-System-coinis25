15a0b8a0cafe79d70728232649adff23887902d9
Lekas27 <miksaleksa32@gmail.com>
2025-03-24 11:37:59 +0100

    Created search filter

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import placeholderImage from "../assets/placeholder.png";
+
 export const CardComponent = ({
   title,
   organizer,
@@ -9,14 +10,14 @@ export const CardComponent = ({
   id,
 }) => {
   return (
-    <div className="bg-darkGray shadow-lg rounded-2xl overflow-hidden w-80 p-4 mx-auto mt-4">
+    <div className="bg-darkGray shadow-lg rounded-2xl overflow-hidden w-80 p-4 mx-auto mt-4 flex flex-col h-full">
       <img
         src={image ? image : placeholderImage}
         alt={title}
         className="w-full h-48 object-cover rounded-xl"
         onError={(e) => (e.target.src = placeholderImage)}
       />
-      <div className="p-4">
+      <div className="p-4 flex flex-col flex-grow">
         <h2 className="text-xl font-semibold text-white">{title}</h2>
         <h4 className="font-semibold text-white">
           Start Date: {new Date(dateStart).toLocaleString()}
@@ -25,11 +26,13 @@ export const CardComponent = ({
           End Date: {new Date(dateEnd).toLocaleString()}
         </h4>
         <p className="text-xl text-lightGray mt-2">{organizer}</p>
-        <Link to={`/parties/${id}`}>
-          <button className="mt-4 px-4 py-2 bg-primaryPurple text-white rounded-lg hover:bg-opacity-80 transition cursor-pointer">
-            Learn More
-          </button>
-        </Link>
+        <div className="mt-auto">
+          <Link to={`/parties/${id}`}>
+            <button className="w-full px-4 py-2 bg-primaryPurple text-white rounded-lg hover:bg-opacity-80 transition cursor-pointer">
+              Learn More
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
