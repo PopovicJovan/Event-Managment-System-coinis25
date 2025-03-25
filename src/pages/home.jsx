6bc16255392a19b0fd3021dabd4d6a52dffdb378
Lekas27 <miksaleksa32@gmail.com>
2025-03-25 11:34:22 +0100

    added overflow hidden on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,23 +4,30 @@ import { NewsFeed } from "../components/newsFeed.jsx";
 import { UpcomingEvents } from "../components/upcomingEvents.jsx";
 import { TopEvents } from "../components/topEvents.jsx";
 
-
 export const HomePage = () => {
   const { parties, isLoading } = useParties();
 
   if (isLoading)
-    return <div className="flex justify-center items-center h-screen text-white">Loading events...</div>;
+    return (
+      <div className="flex justify-center items-center h-screen text-white">
+        Loading events...
+      </div>
+    );
 
   return (
-    <div className="container mx-auto px-4 py-8 min-h-screen bg-black text-white">
+    <div className="container mx-auto px-4 py-8 min-h-screen bg-black text-white overflow-hidden">
       {/* Header */}
       <header className="mb-12">
-        <h1 className="text-4xl text-purple-400 font-bold text-center mb-4">Event Management System</h1>
-        <p className="text-xl text-center text-white-400">Discover amazing events happening worldwide</p>
+        <h1 className="text-4xl text-purple-400 font-bold text-center mb-4">
+          Event Management System
+        </h1>
+        <p className="text-xl text-center text-white-400">
+          Discover amazing events happening worldwide
+        </p>
       </header>
 
       {/* Slider & NewsFeed Section */}
-      <div className="flex flex-col lg:flex-row gap-8">
+      <div className="flex flex-col lg:flex-row gap-8 ">
         <Slider parties={parties} />
         <NewsFeed parties={parties} />
       </div>
@@ -28,8 +35,6 @@ export const HomePage = () => {
       <TopEvents events={parties} />
       {/* Upcoming Events Section */}
       <UpcomingEvents parties={parties} />
-
-      
     </div>
   );
 };
